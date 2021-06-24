b645d1924fdb4814abca5cf4e485ef1bcb6350a6
Vanderlei Loureiro <vanderloureiro.dev@gmail.com>
2021-06-24 11:08:31 -0300

    Percorrendo array generico de publicacoes

--- search.js
+++ search.js
@@ -23,5 +23,20 @@ function getPublications() {
 }
 
 function feelFeed(data) {
+
+    const publicationsFeed = document.getElementById('publications-feed-list');
+
+    console.log(publicationsFeed);
     console.log(data);
+
+    data.items.forEach(item => {
+        var content = '<div class="publication-card" id="publication-card">'
+        content += '<img src="assets/retorno.jpeg" alt="">'
+        content += '<div class="publication-card-text"><a href="#"><h4>Retorno amigável de exceções no Spring com ExceptionHandler</h4></a>'
+        content += '<p>Eliminando retornos de erro gigantes em requisições Spring com ExceptionHandler e ControllerAdvice</p>'
+        content += '<p class="date">17/03/2021</p>'
+        content += '</div></div>'
+        
+        publicationsFeed.insertAdjacentHTML('afterend', content);
+    })
 }
