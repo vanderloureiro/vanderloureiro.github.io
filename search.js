a64e0afde40c4a93c13e637ddad19f54f2d8201e
Vanderlei Loureiro <vanderloureiro.dev@gmail.com>
2021-06-24 11:38:44 -0300

    Inserindo foto, titulo e data em publicacoes

--- search.js
+++ search.js
@@ -3,13 +3,8 @@ getPublications()
 
 function getPublications() {
 
-    var myHeaders = new Headers();
-
-    var myInit = { method: 'GET',
-                headers: myHeaders};
-
     const URL_TO_FETCH = 'https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@vanderloureiro';
-    fetch(URL_TO_FETCH, myInit)
+    fetch(URL_TO_FETCH, { method: 'GET'})
         .then(function(response) {
             if (response.status == 200) {
                 response.json().then(function(data) {
@@ -22,21 +17,31 @@ function getPublications() {
         });
 }
 
+function orderPublicationsListByDate(publications) {
+    return publications;
+}
+
 function feelFeed(data) {
 
     const publicationsFeed = document.getElementById('publications-feed-list');
 
-    console.log(publicationsFeed);
-    console.log(data);
+    const publications = orderPublicationsListByDate(data.items);
 
-    data.items.forEach(item => {
+    publications.forEach(item => {
         var content = '<div class="publication-card" id="publication-card">'
-        content += '<img src="assets/retorno.jpeg" alt="">'
-        content += '<div class="publication-card-text"><a href="#"><h4>Retorno amigável de exceções no Spring com ExceptionHandler</h4></a>'
+        if (item.thumbnail) {
+            content += '<img src="'+item.thumbnail+'" alt="">'
+        }
+        content += '<div class="publication-card-text"><a href="'+item.link+'" target="_blank"><h4>'+item.title+'</h4></a>'
         content += '<p>Eliminando retornos de erro gigantes em requisições Spring com ExceptionHandler e ControllerAdvice</p>'
-        content += '<p class="date">17/03/2021</p>'
+        content += '<p class="date">'+customizeDate(item.pubDate)+'</p>'
         content += '</div></div>'
         
         publicationsFeed.insertAdjacentHTML('afterend', content);
     })
+}
+
+function customizeDate(date) {
+    console.log(date);
+    return date;
 }
