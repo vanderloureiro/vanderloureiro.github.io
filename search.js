68fb29b699ee1c0939dcd0241f85485dcf21aa89
Vanderlei Loureiro <vanderloureiroleite@gmail.com>
2025-04-20 23:24:07 -0300

    reducao busca

--- search.js
+++ search.js
@@ -4,20 +4,20 @@ getPublications()
 function getPublications() {
 
     const URL_TO_FETCH = 'https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@vanderloureiro';
-    fetch(URL_TO_FETCH, { method: 'GET'})
-        .then(function(response) {
+    fetch(URL_TO_FETCH, { method: 'GET' })
+        .then(function (response) {
             if (response.status == 200) {
-                response.json().then(function(data) {
+                response.json().then(function (data) {
                     feelFeed(data);
                 });
             }
         })
-        .catch(function(err) {});
+        .catch(function (err) { });
 }
 
 function orderPublicationsListByDate(publications) {
 
-    publications.sort(function(pub1, pub2) {
+    publications.sort(function (pub1, pub2) {
         if (pub1.pubDate > pub2.pubDate) return 1;
         return -1;
     });
@@ -30,17 +30,20 @@ function feelFeed(data) {
 
     const publications = orderPublicationsListByDate(data.items);
 
-    publications.forEach(item => {
-        var content = '<div class="publication-card" id="publication-card">'
-        if (item.thumbnail) {
-            content += '<img src="'+item.thumbnail+'" alt="">'
+    publications.forEach((item, index) => {
+        // buscando os 5 últimos
+        if (index > 4) {
+            var content = '<div class="publication-card" id="publication-card">'
+            if (item.thumbnail) {
+                content += '<img src="' + item.thumbnail + '" alt="">'
+            }
+            content += '<div class="publication-card-text"><a href="' + item.link + '" target="_blank"><h4>' + item.title + '</h4></a>'
+            content += '<p>' + shortenText(toText(item.content), 0, 130) + '...</p>'
+            content += '<p class="date">' + customizeDate(item.pubDate) + '</p>'
+            content += '</div></div>'
+
+            publicationsFeed.insertAdjacentHTML('afterend', content);
         }
-        content += '<div class="publication-card-text"><a href="'+item.link+'" target="_blank"><h4>'+item.title+'</h4></a>'
-        content += '<p>'+shortenText(toText(item.content),0, 130)+ '...</p>'
-        content += '<p class="date">'+customizeDate(item.pubDate)+'</p>'
-        content += '</div></div>'
-        
-        publicationsFeed.insertAdjacentHTML('afterend', content);
     })
 }
 
@@ -61,12 +64,12 @@ function removeImageCredit(content) {
     return content;
 }
 
-function shortenText(text,startingPoint ,maxLength) {
-    return text.length > maxLength? text.slice(startingPoint, maxLength) : text
+function shortenText(text, startingPoint, maxLength) {
+    return text.length > maxLength ? text.slice(startingPoint, maxLength) : text
 }
- 
+
 function customizeDate(date) {
-    date = shortenText(date,0,10);
-    const formatedDate = date.slice(8, 10) +'/'+ date.slice(5, 7) +'/'+ date.slice(0, 4);
+    date = shortenText(date, 0, 10);
+    const formatedDate = date.slice(8, 10) + '/' + date.slice(5, 7) + '/' + date.slice(0, 4);
     return formatedDate;
 }
